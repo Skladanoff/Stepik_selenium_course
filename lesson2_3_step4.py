1e57fcc7ec03b226bcef2afca7705091975845cd
Skladanoff <skladanov@yandex.ru>
2019-12-22 19:01:18 +0400

    homework week 2

---
+++ lesson2_3_step4.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+import math
+import time
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/alert_accept.html"
+
+try:
+
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    button1 = browser.find_element_by_class_name("btn")
+    button1.click()
+
+    confim = browser.switch_to.alert
+    confim.accept()
+
+    x = browser.find_element_by_id("input_value").text
+    y = calc(x)
+
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+    button2 = browser.find_element_by_class_name("btn")
+    button2.click()
+
+finally:
+    time.sleep(10)
+    browser.quit()
